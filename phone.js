2d820813ef77de5ce525ab6fd717d781951952bc
VARVISHNU08 <91448991+VARVISHNU08@users.noreply.github.com>
2022-05-01 11:02:27 +0530

    Add files via upload

---
+++ phone.js
@@ -0,0 +1,29 @@
+cocoSsdStatus = "";
+
+function preload(){
+    img = loadImage('Phone.jpg');
+}
+
+function setup(){
+    canvas = createCanvas(640, 480);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+
+    document.getElementById("status").innerHTML = "Detecting Objects";
+}
+
+function modelLoaded(){
+    cocoSsdStatus = true;
+
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+    }
+}
